ef7e96d4fba4250b99109cd2b5821625995382ad
Güere Casas Jose Manuel <113396969+JGuere2811@users.noreply.github.com>
2025-09-08 07:08:05 -0500

    otra linea de comandos

---
+++ UdemyPythonCeroaExperto/ProyectoInicial/Operadores_en_python/Operadores_comparacion/operadores_comparacion.py
@@ -0,0 +1,28 @@
+print("** Operadores de comparacion (Relaciones)**")
+a, b = 10, 5
+print(f"Valor de a: {a} , b: {b}")
+# operador ==
+resultado = (a == b)
+print(f"El resultado de a == b es: {resultado}")
+# operador !=
+resultado = (a != b)
+print(f"El resultado de a != b es: {resultado}")
+# operador >
+resultado = (a > b)
+print(f"El resultado de a > b es: {resultado}")
+# operador <
+resultado = (a < b)
+print(f"El resultado de a < b es: {resultado}")
+# operador >=
+resultado = (a >= b)
+print(f"El resultado de a >= b es: {resultado}")
+# operador <=
+resultado = (a <= b)
+print(f"El resultado de a <= b es: {resultado}")
+# Comparación de cadenas
+cadena1 = "Hola"
+cadena2 = "Mundo"
+resultado = (cadena1 == cadena2)
+print(f"El resultado de cadena1 == cadena2 es: {resultado}")
+resultado = (cadena1 != cadena2)
+print(f"El resultado de cadena1 != cadena2 es: {resultado}")
